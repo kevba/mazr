1201702f7f1cd7eeec3f293c8efa8de645fc876d
Kevin Batema <kevinbatema@gmail.com>
2018-08-20 23:36:33 +0200

    Very much WIP

--- src/app.js
+++ src/app.js
@@ -3,27 +3,18 @@ import React from 'react';
 import {HashRouter} from 'react-router-dom';
 
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
 
 // import Paper from 'material-ui/Paper';
 import {MuiThemeProvider} from '@material-ui/core/styles';
+
+import {store} from 'src/store';
 
 import Header from 'src/header';
 
 import GameView from 'src/gameView';
 
 import baseTheme from 'src/themes/baseTheme';
-import reducer from 'src/reducers';
 
-const store = createStore(
-    reducer,
-    {},
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
 
 class App extends React.Component {
     constructor(props) {
