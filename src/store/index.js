00ca504c7f56d97521a51cf3be4124e137708a71
Kevin Batema <kevinbatema@gmail.com>
2018-08-25 22:35:58 +0200

    Removed redux debugging tools

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,7 @@ export const store = createStore(
     reducer,
     {},
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
     )
 );
 
